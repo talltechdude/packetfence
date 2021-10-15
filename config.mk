2fcba9c00d2ec8ebf23453ba47bc45d22c8bd9d4
James Rouzier <jrouzier@inverse.ca>
2021-10-15 08:59:36 -0400

    Upgrade to go1.17.2

--- config.mk
+++ config.mk
@@ -31,7 +31,7 @@ SRC_WEBSITE_DIR = $(SRC_ROOT_DIR)/website
 #
 # Golang
 #
-GOVERSION = go1.17.1
+GOVERSION = go1.17.2
 PF_BINARIES = pfhttpd pfdhcp pfdns pfstats pfdetect galera-autofix pfacct pfcertmanager pfcron
 
 #
